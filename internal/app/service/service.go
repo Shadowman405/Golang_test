4a90f9c98b8f343addb0bd9996182f8caaf234b1
Shadowman405 <maximus405@gmail.com>
2023-05-04 23:31:43 +0300

    layout

---
+++ internal/app/service/service.go
@@ -0,0 +1,17 @@
+package service
+
+import "time"
+
+type Service struct {
+}
+
+func New() *Service {
+	return &Service{}
+}
+
+func (s *Service) DaysLeft() int64 {
+	date := time.Date(2025, time.April, 1, 0, 0, 0, 0, time.UTC)
+	duration := time.Until(date)
+
+	return int64(duration.Hours()) / 24
+}
